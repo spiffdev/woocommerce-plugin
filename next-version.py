1fc25ae694151759aeb5e530c864c4a6440c7713
rheber <robert@spiff.com.au>
2021-12-02 09:11:43 +1100

    tweak version script to take stdin

--- next-version.py
+++ next-version.py
@@ -1,8 +1,11 @@
-with open("./VERSION") as versionFile:
-    content = versionFile.read()
-    parts = content.split('.')
-    if len(parts) < 3:
-        raise Exception("Only {1} parts in version number {0}".format(content, len(parts)))
-    parts[2] = str(int(parts[2]) + 1)
-    newVersion = '.'.join(parts)
+# To run: python2 next-version.py < VERSION
+
+import sys
+
+content = sys.stdin.read()
+parts = content.split('.')
+if len(parts) < 3:
+    raise Exception("Only {1} parts in version number {0}".format(content, len(parts)))
+parts[2] = str(int(parts[2]) + 1)
+newVersion = '.'.join(parts)
 print newVersion
