f724097fb2570edc24120719295dbac6dab93715
rheber <robert@spiff.com.au>
2021-12-02 09:01:50 +1100

    check version parts

--- next-version.py
+++ next-version.py
@@ -1,6 +1,8 @@
 with open("./VERSION") as versionFile:
     content = versionFile.read()
     parts = content.split('.')
+    if len(parts) < 3:
+        raise Exception("Only {1} parts in version number {0}".format(content, len(parts)))
     parts[2] = str(int(parts[2]) + 1)
     newVersion = '.'.join(parts)
 print newVersion
