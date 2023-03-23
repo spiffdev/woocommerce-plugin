0f7a4702ed93cb04a638701ad64aed565c3bf8b5
Robert Heber <rheber@rediffmail.com>
2023-03-24 09:46:11 +1100

    Get transactions using graphql (#9)
    * update py script
    
    * get transaction with gql
    
    * replace script takes path as arg
    
    * defined check
    
    ---------
    
    Co-authored-by: rheber <robert@spiff.com.au>

--- replace-env-vars.py
+++ replace-env-vars.py
@@ -1,14 +1,22 @@
+"""
+Python2 script that replaces environment-dependent constants in the PHP files.
+"""
+
 import sys
 
 env = sys.argv[1]
+path = sys.argv[2]
 
-with open("./spiff-connect/spiff-connect.php") as spiffConnect:
-    lines = spiffConnect.readlines()
+def replaceConstants(file):
+    lines = file.readlines()
     for line in lines:
-        if line.startswith('define("SPIFF_API_BASE"'):
+        if line.find('define("SPIFF_API_BASE"') > -1:
             if (env == 'prod'):
                 print 'define("SPIFF_API_BASE", "https://api.spiff.com.au");'
             else:
                 print 'define("SPIFF_API_BASE", "https://api.app.dev.spiff.com.au");'
         else:
             print line,
+
+with open(path) as file:
+    replaceConstants(file)
